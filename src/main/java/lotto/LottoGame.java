f4d65eb148ff0aaa46de78b384dc277c0a0a3eb5
PZacheja <przemek.zacheja@gmail.com>
2022-02-05 15:19:41 +0100

    Refactor to Set collection for numbers

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,66 +1,59 @@
 package lotto;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class LottoGame {
 
-    public static final String JESZCZE_RAZ = "liczba z poza zakresu 1-99, spróbuj jeszcze raz";
-    public static final String WYBIERZ_INNA = "Już wybrałeś tą liczbę, wybierz inną";
-    public static final String CZY_WYGRALES = "Sprawdzam czy wygrałeś: ... ";
-    public static final String WYBRALES_LICZBY = "wybrałeś liczby: ";
-    public static final String WYLOSOWANE_LICZBY_TO = "Wylosowane liczby to: ";
-    public static final String GRATULACJE = "Gratulacje";
-    public static final String SPROBUJ_JESZCZE_RAZ = "Spróbuj jeszcze raz";
-    public static final int MAX_DIGIT = 99;
+    private static final String JESZCZE_RAZ = "liczba z poza zakresu 1-99, spróbuj jeszcze raz";
+    private static final String CZY_WYGRALES = "Sprawdzam czy wygrałeś: ... ";
+    private static final String WYBRALES_LICZBY = "wybrałeś liczby: ";
+    private static final String WYLOSOWANE_LICZBY_TO = "Wylosowane liczby to: ";
+    private static final String GRATULACJE = "Gratulacje";
+    private static final String SPROBUJ_JESZCZE_RAZ = "Spróbuj jeszcze raz";
+    private static final int MAX_ZAKRES = 99;
+    private static final int MIN_ZAKRES = 1;
     private static Random rand = new Random();
-    private static final int NUMBERS_OFPRANDOMS = 6;
 
-    private static int[] playerChoices = new int[NUMBERS_OFPRANDOMS];
-    private static int[] randomsNumbers = new int[NUMBERS_OFPRANDOMS];
+
+    private static Set<Integer> playerChoicesSet = new HashSet<>();
+    private static Set<Integer> randomsNumbersSet = new HashSet<>();
     private Scanner scanner = new Scanner(System.in);
-
 
 
     public void play() {
         readNumbersFromPlayer(scanner);
+        scanner.close();
+        generateRandomNumber1To99(randomsNumbersSet);
+        checkTheWinMessage();
+    }
 
-        System.out.println(WYBRALES_LICZBY + printTable(playerChoices));
-
-        generateRandomNumber1To99(randomsNumbers);
-        System.out.println(WYLOSOWANE_LICZBY_TO + printTable(randomsNumbers));
-
+    private void checkTheWinMessage() {
         System.out.println(CZY_WYGRALES);
-
-        System.out.println(isWinTheGame(playerChoices, randomsNumbers) ? GRATULACJE : SPROBUJ_JESZCZE_RAZ);
+        System.out.println(isWinTheGame(playerChoicesSet, randomsNumbersSet) ? GRATULACJE : SPROBUJ_JESZCZE_RAZ);
     }
 
 
-
-    private static boolean isWinTheGame(int[] playerChoices, int[] randomsNumbers) {
+    private static boolean isWinTheGame(Set<Integer> playerChoices, Set<Integer> randomsNumbers) {
         for (int playerChoice : playerChoices) {
-            for (int randomsNumber : randomsNumbers) {
-                if (playerChoice != randomsNumber) {
-                    return false;
+            for(int randomsNumber: randomsNumbers){
+                if (playerChoice == randomsNumber) {
+                    return true;
                 }
             }
         }
-        return true;
+        return false;
     }
 
 
-    private static void generateRandomNumber1To99(int[] randomsNumbers) {
-
-        for (int i = 0; i < randomsNumbers.length; i++) {
-            int checkDigit = rand.nextInt(MAX_DIGIT) + 1;
-            while (isTheNumberExistInTable(checkDigit, randomsNumbers)) {
-                checkDigit = rand.nextInt(MAX_DIGIT) + 1;
-            }
-            randomsNumbers[i] = checkDigit;
+    private static void generateRandomNumber1To99(Set<Integer> randomsNumbersSet) {
+        while (randomsNumbersSet.size() < 6) {
+            int checkDigit = rand.nextInt(MAX_ZAKRES) + MIN_ZAKRES;
+            randomsNumbersSet.add(checkDigit);
         }
+        System.out.println(WYLOSOWANE_LICZBY_TO + printTable(randomsNumbersSet));
     }
 
-    private static String printTable(int[] playerChoices) {
+    private static String printTable(Set<Integer> playerChoices) {
         StringBuilder line = new StringBuilder();
         for (int i : playerChoices) {
             line.append(i).append(" ");
@@ -69,31 +62,18 @@ public class LottoGame {
     }
 
     private static void readNumbersFromPlayer(Scanner scanner) {
-        for (int i = 0; i < 6; i++) {
-            int digit = scanner.nextInt();
-            while (true) {
-                if (digit < 1 || digit > MAX_DIGIT) {
-                    System.out.println(JESZCZE_RAZ);
-                } else {
-                    if (isTheNumberExistInTable(digit, playerChoices)) {
-                        System.out.println(WYBIERZ_INNA);
-                    } else {
-                        playerChoices[i] = digit;
-                        break;
-                    }
-                }
-                digit = scanner.nextInt();
+        int digit;
+        while (playerChoicesSet.size() < 6) {
+            digit = scanner.nextInt();
+            if (digit < MIN_ZAKRES || digit > MAX_ZAKRES) {
+                System.out.println(JESZCZE_RAZ);
+            } else {
+                playerChoicesSet.add(digit);
             }
         }
+        System.out.println(WYBRALES_LICZBY + printTable(playerChoicesSet));
     }
 
-    private static boolean isTheNumberExistInTable(int digit, int[] playerChoices) {
-        for (int playerChoice : playerChoices) {
-            if (digit == playerChoice) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
+
 
